1da9af2a1c0627aab1cf31fdfe9e24ff9de5bf0f
renzhenming <1565766177@qq.com>
2023-04-12 08:35:21 +0800

    trace

--- app/src/main/java/com/rzm/matrix_testing/MyApplication.java
+++ app/src/main/java/com/rzm/matrix_testing/MyApplication.java
@@ -3,11 +3,14 @@ package com.rzm.matrix_testing;
 import android.app.Application;
 
 import com.tencent.matrix.Matrix;
-import com.tencent.matrix.iocanary.IOCanaryPlugin;
-import com.tencent.matrix.iocanary.config.IOConfig;
+import com.tencent.matrix.trace.TracePlugin;
+import com.tencent.matrix.trace.config.TraceConfig;
+import com.tencent.matrix.util.MatrixLog;
+
+import java.io.File;
 
 public class MyApplication extends Application {
-
+    private static final String TAG = "Matrix.Application";
     @Override
     public void onCreate() {
         super.onCreate();
@@ -15,17 +18,53 @@ public class MyApplication extends Application {
         builder.pluginListener(new TestPluginListener(this)); // add general pluginListener
         DynamicConfigImplDemo dynamicConfig = new DynamicConfigImplDemo(); // dynamic config
 
-        // init plugin
-        IOCanaryPlugin ioCanaryPlugin = new IOCanaryPlugin(new IOConfig.Builder()
-                .dynamicConfig(dynamicConfig)
-                .build());
-        //add to matrix
-        builder.plugin(ioCanaryPlugin);
+        // Configure trace canary.
+        TracePlugin tracePlugin = configureTracePlugin(dynamicConfig);
+
+        builder.plugin(tracePlugin);
 
         //init matrix
         Matrix.init(builder.build());
+    }
 
-        // start plugin
-        ioCanaryPlugin.start();
+
+    private TracePlugin configureTracePlugin(DynamicConfigImplDemo dynamicConfig) {
+
+        boolean fpsEnable = dynamicConfig.isFPSEnable();
+        boolean traceEnable = dynamicConfig.isTraceEnable();
+        boolean signalAnrTraceEnable = dynamicConfig.isSignalAnrTraceEnable();
+
+        File traceFileDir = new File(getApplicationContext().getFilesDir(), "matrix_trace");
+        if (!traceFileDir.exists()) {
+            if (traceFileDir.mkdirs()) {
+                MatrixLog.e(TAG, "failed to create traceFileDir");
+            }
+        }
+
+        File anrTraceFile = new File(traceFileDir, "anr_trace");    // path : /data/user/0/sample.tencent.matrix/files/matrix_trace/anr_trace
+        File printTraceFile = new File(traceFileDir, "print_trace");    // path : /data/user/0/sample.tencent.matrix/files/matrix_trace/print_trace
+
+        TraceConfig traceConfig = new TraceConfig.Builder()
+                .dynamicConfig(dynamicConfig)
+                .enableFPS(fpsEnable)
+                .enableEvilMethodTrace(traceEnable)
+                .enableAnrTrace(traceEnable)
+                .enableStartup(traceEnable)
+                .enableIdleHandlerTrace(traceEnable)                    // Introduced in Matrix 2.0
+                .enableMainThreadPriorityTrace(true)                    // Introduced in Matrix 2.0
+                .enableSignalAnrTrace(signalAnrTraceEnable)             // Introduced in Matrix 2.0
+                .anrTracePath(anrTraceFile.getAbsolutePath())
+                .printTracePath(printTraceFile.getAbsolutePath())
+                .splashActivities("com.rzm.matrix_testing.SplashActivity;")
+                .enableHistoryMsgRecorder(true)
+                .enableDenseMsgTracer(true)
+                .isDebug(true)
+                .isDevEnv(true)
+                .build();
+
+        //Another way to use SignalAnrTracer separately
+        //useSignalAnrTraceAlone(anrTraceFile.getAbsolutePath(), printTraceFile.getAbsolutePath());
+
+        return new TracePlugin(traceConfig);
     }
 }
---
+++ app/src/main/java/com/rzm/matrix_testing/trace/StartUpBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.rzm.matrix_testing.trace;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.tencent.matrix.util.MatrixLog;
+
+public class StartUpBroadcastReceiver extends BroadcastReceiver {
+    private static final String TAG = "Matrix.StartUpBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        MatrixLog.i(TAG, "[onReceive]");
+        context.startActivity(new Intent(context, TestOtherProcessActivity.class));
+    }
+}
